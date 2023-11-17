ab6a90bb2f784467902fbc5fda95cfde31c0cae5
agusmena <agusmena60@mail.com>
2023-11-17 10:37:55 -0300

    add change ready to upload to cloud

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
-
-const url = 'mongodb://127.0.0.1:27017/crudBurgers65i'
-mongoose.connect(url);
+import 'dotenv/config' 
+//const url = 'mongodb://127.0.0.1:27017/crudBurgers65i'
+const URI = process.env.MONGODB_URI
+mongoose.connect(URI);
 
 const conection = mongoose.connection;
 conection.once('open',()=>{
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import path from "path";
 import './database';
 import router from "./routes/products.routes";
-
+import 'dotenv/config' 
 //console.log("desde mi backend")
 //creo la instancia de Express
 
